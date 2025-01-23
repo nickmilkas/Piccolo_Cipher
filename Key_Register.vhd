f146afcfc39aff9972d7304cba92394696812911
nickmilkas <nickmilkas@gmail.com>
2025-01-24 00:00:39 +0200

    Add 'pictures' folder for readme file

--- Key_Register.vhd
+++ Key_Register.vhd
@@ -2,38 +2,55 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity Register_File is
+entity key_reg is
     port (
-        clk : in std_logic;
-        rst : in std_logic;
-        write_data : in std_logic_vector (15 downto 0);
-        read_addr0, read_addr1 : in std_logic_vector (6 downto 0);
-        data_1, data_2 : out std_logic_vector (15 downto 0);
-        write_enable : in std_logic
+        clk, rst      : in  std_logic;
+        write_data    : in  std_logic_vector (31 downto 0);
+        read_addr0    : in  std_logic_vector (5 downto 0);
+        read_addr1    : in  std_logic_vector (5 downto 0);
+        write_enable  : in  std_logic;
+        read_enable   : in  std_logic;
+        data_1, data_2: out std_logic_vector (31 downto 0);
+        is_full       : out std_logic
     );
-end Register_File;
+end key_reg;
 
-architecture Behavioral of Register_File is
-    type reg_array is array (0 to 65) of std_logic_vector (15 downto 0);
-    signal registers: reg_array := (others => (others => '0'));
-    signal write_addr: std_logic_vector(6 downto 0) := "0000000";
-
+architecture key_reg_arch of key_reg is
+    type reg_array is array (0 to 32) of std_logic_vector (31 downto 0);
+    signal registers  : reg_array := (others => (others => '0'));
+    signal write_addr : std_logic_vector(5 downto 0) := (others => '0');
+    signal full_flag  : std_logic := '0';
 begin
 
     process(clk, rst)
     begin
         if rst = '1' then
-            registers <= (others => (others => '0'));
-            write_addr <= "0000000";
-        elsif clk'event and clk = '1' then
+            registers   <= (others => (others => '0'));
+            write_addr  <= (others => '0');
+            full_flag   <= '0';
+        elsif rising_edge(clk) then
             if write_enable = '1' then
                 registers(to_integer(unsigned(write_addr))) <= write_data;
+                if unsigned(write_addr) < 32 then
+                    write_addr <= std_logic_vector(unsigned(write_addr) + 1);
+                else
+                    full_flag <= '1';
+                end if;
             end if;
-            write_addr <= std_logic_vector(unsigned(write_addr) + 1);
         end if;
     end process;
-    
-    data_1 <= registers(to_integer(unsigned(read_addr0)));
-    data_2 <= registers(to_integer(unsigned(read_addr1)));
 
-end Behavioral;
+    is_full <= full_flag;
+
+    process(clk, rst)
+    begin
+        if read_enable = '1' then
+            data_1 <= registers(to_integer(unsigned(read_addr0)));
+            data_2 <= registers(to_integer(unsigned(read_addr1)));
+        else
+            data_1 <= (others => '0');
+            data_2 <= (others => '0');
+        end if;
+    end process;
+
+end key_reg_arch;
