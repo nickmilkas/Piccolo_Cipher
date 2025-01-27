8d1c0c8be18500725229799d41d4e475d857e7de
nickmilkas <nickmilkas@gmail.com>
2025-01-28 00:24:07 +0200

    Parametrized register

--- Register.vhd
+++ Register.vhd
@@ -1,28 +1,27 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
-entity iter_reg is
+entity reg_nbit is
+    generic(WIDTH : integer := 16);
     port(
-        clk   : in std_logic;
-        reset : in std_logic;
-        d     : in std_logic_vector(63 downto 0);
-        q     : out std_logic_vector(63 downto 0)
+        clk     : in  std_logic;
+        rst     : in  std_logic;
+        load_en : in  std_logic;
+        input_bits       : in  std_logic_vector(WIDTH-1 downto 0);
+        output_bits       : out std_logic_vector(WIDTH-1 downto 0)
     );
-end iter_reg;
+end reg_nbit;
 
-architecture iter_reg_arch of iter_reg is
-    signal reg_data : std_logic_vector(63 downto 0) := (others => '0');
-	
-	begin
-		process(clk, reset)
-		begin
-			if reset = '1' then
-				reg_data <= (others => '0');
-			elsif rising_edge(clk) then
-				reg_data <= d;
-			end if;
-		end process;
-
-		q <= reg_data;
-	end iter_reg_arch;
+architecture reg_nbit_arch of reg_nbit is
+begin
+    process(clk, rst)
+    begin
+        if rst = '1' then
+            output_bits <= (others => 'X');
+        elsif rising_edge(clk) then
+            if load_en = '1' then
+                output_bits <= input_bits;
+            end if;
+        end if;
+    end process;
+end reg_nbit_arch;
